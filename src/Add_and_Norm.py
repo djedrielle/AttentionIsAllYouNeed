b315908784170fa5cac7264b028d05023565ae39
djedrielle <d.alexander.1@estudiantec.cr>
2026-06-25 14:41:54 -0500

    Add and Norm added

---
+++ src/Add_and_Norm.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class AddAndNorm(nn.Module):
+    def __init__(self, d_model, dropout):
+        super().__init__()
+        self.norm = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer_output):
+        return self.norm(x + self.dropout(sublayer_output))
